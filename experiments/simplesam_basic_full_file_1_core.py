e52bc369f6e8818b2a2169383ff21fc8054aefcb
Jack Tierney <jack.tierney@student.ucc.ie>
2023-04-12 06:48:07 +0100

    pysam and simplesam setup

---
+++ experiments/simplesam_basic_full_file_1_core.py
@@ -0,0 +1,70 @@
+'''
+Experiment where we use pysam to read a bam file and print out the
+number of reads in the file.
+'''
+import resource
+import time
+import os
+
+from simplesam import Reader, Writer
+import pysam
+import argparse
+from rich import inspect
+
+def parse_bam(bam_file: str) -> dict:
+    '''
+    This function will parse a BAM file and return a dictionary of the reads.
+    '''
+    read_dict = {}
+    # Open the BAM file as SAM
+    file = pysam.view(bam_file)
+    print(type(file))
+
+    read_dict = {}
+    # Open the BAM file
+    for read in Reader(file):
+        if read.query_name not in read_dict:
+            read_dict[read.query_name] = {
+                        'read_name': read.query_name,
+                        'read_length': read.query_length,
+                        'reference_name': read.reference_name,
+                        'reference_start': read.reference_start,
+                        'reference_end': read.reference_end,
+                        'sequence': read.query_sequence,
+                        'sequence_qualities': read.query_qualities,
+                        'tags': read.tags,
+                    }
+
+
+    return read_dict
+
+
+def main(args):
+    '''
+    Main function for the script. This function will execute all experiments found in the
+    experiments directory and output the results to a file.
+    '''
+    start_time = time.time()
+
+    read_dict = parse_bam(args.bam_file)
+
+    end_time = time.time()
+
+    print("CPU usage:", resource.getrusage(resource.RUSAGE_SELF).ru_utime)
+    print("Memory usage:", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+    print("Execution time:", end_time - start_time)
+
+    with open('results.csv', 'a') as f:
+        f.write('experiment_name,input_bam,num_reads,max_mem,max_cpu,time\n')
+        f.write(f"{os.path.basename(__file__).strip('.py')},{args.bam_file},{len(read_dict)},"
+                f"{resource.getrusage(resource.RUSAGE_SELF).ru_maxrss},"
+                f"{resource.getrusage(resource.RUSAGE_SELF).ru_utime},"
+                f"{end_time - start_time}\n")
+        
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bam_file', type=str, default='data/NA12878.bam',
+                        help='The BAM file to read')
+    args = parser.parse_args()
+    main(args)
